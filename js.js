a072f93908922a04b39a39afdc8f9f68be3b23c4
Victor Vilar <victor.svilar@gmail.com>
2022-03-02 13:36:06 -0300

    criando area de atributos conforme a imagem.

--- js.js
+++ js.js
@@ -1,19 +1,3 @@
-function addActionListenerScreenshotsButtons(){
-    var bts = [];
-    bts = document.querySelectorAll('.button-screenshots');
-    bts.forEach((item) => {
-        item.addEventListener("mouseover",() =>{
-            item.style.fontSize = "40px";
-        });
-        item.addEventListener("mouseout",() =>{
-            item.style.fontSize = "60px";
-        })
-    })
-
-}
-
-
- addActionListenerScreenshotsButtons();
 
 
 
