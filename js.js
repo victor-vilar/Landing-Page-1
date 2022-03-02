248e9e46ec9be2de252bead71c7825a126cb776f
Victor Vilar <victor.svilar@gmail.com>
2022-03-02 14:37:48 -0300

    adicionando seção de depoimentos.

--- js.js
+++ js.js
@@ -2,3 +2,18 @@
 
 
 
+function diminuirTamanhodoButaoAoClicar(){
+
+const butoes = document.querySelectorAll('.button-screenshots');
+
+    butoes.forEach(item =>{
+        item.addEventListener('click', () => {
+            item.style.fontSize = "40px";
+            
+        })
+    })
+
+}
+
+
+diminuirTamanhodoButaoAoClicar();
