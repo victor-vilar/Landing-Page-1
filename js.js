b994b56ab8928d7a6eed4ce1116265ebc6dc47ca
Victor Vilar <victor.svilar@gmail.com>
2022-03-05 15:18:56 -0300

    adicionando link do fontawesome.com.

--- js.js
+++ js.js
@@ -4,16 +4,19 @@
 
 function diminuirTamanhodoButaoAoClicar(){
 
-const butoes = document.querySelectorAll('.button-screenshots');
+    const butoes = document.querySelectorAll('.button-screenshots');
 
-    butoes.forEach(item =>{
-        item.addEventListener('click', () => {
-            item.style.fontSize = "40px";
-            
+        butoes.forEach(item =>{
+            item.addEventListener('click', () => {
+                item.style.fontSize = "40px";
+                
+            })
         })
-    })
 
 }
+
+
+
 
 
 diminuirTamanhodoButaoAoClicar();
